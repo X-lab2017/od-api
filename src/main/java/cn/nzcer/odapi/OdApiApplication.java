5301da34d249b369be6c8db0fa611e9337da0b25
nzcer <ningzhicheng1215@qq.com>
2022-12-01 14:26:14 +0800

    feat: initial commit

---
+++ src/main/java/cn/nzcer/odapi/OdApiApplication.java
@@ -0,0 +1,13 @@
+package cn.nzcer.odapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/nzcer/odapi/OdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.nzcer.odapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
